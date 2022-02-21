ea1e65506c79f1fc578b6abfee33bf885fd489e9
papa-react <makiya112545@gmail.com>
2022-02-21 10:34:15 +0300

    add domain name and image format

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['cdn.sanity.io'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    formats: ['image/avif', 'image/webp'],
+  },
 }
