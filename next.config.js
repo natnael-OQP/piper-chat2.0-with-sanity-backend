bb11ef2072d1ab4378fec966886ff99664f88fea
papa-react <makiya112545@gmail.com>
2022-02-23 16:35:57 +0300

    add domain name

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['cdn.sanity.io'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    formats: ['image/avif', 'image/webp'],
-  },
-}
+	reactStrictMode: true,
+	images: {
+		domains: ["cdn.sanity.io", "lh3.googleusercontent.com"],
+		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+		formats: ["image/avif", "image/webp"],
+	},
+};
